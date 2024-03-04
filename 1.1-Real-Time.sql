56fe9e864bf112528e3fa346cbf805cda7c71c4f
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-04 10:49:30 -0300

    Created 1.1-Real-Time.sql
    inicio read

---
+++ 1.1-Real-Time.sql
@@ -0,0 +1,18 @@
+/*  Optimizing for performace, scalability and real-time insights */
+
+-- Companies needs real-time insights on the transaction data.
+
+-- Microsoft SQLServer
+--      high-performance transaction processing while simultaneusly allowing real-time analytics
+--          without the need for a separate set of ETL processes, a separate data warehouse
+
+-- Microsoft SQLServer 2019
+--      is built on a database engine that is number one for
+--          TPC-E   (On-Line Transaction Processing Benchmark)
+--          TPC-H   (Decision Support Benchmark)
+--          PMEM    (Persistent Memory)
+--          SCM     (Storage Class Memory)
+--          IQP     (Intelligent Query Processing)
+
+-- Hybrid transactional and analytical processing (HTAP)
+--pag.31/489
