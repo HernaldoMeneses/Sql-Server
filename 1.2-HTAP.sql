5ee25c9b58f3d34055cb52ca1bd2cb85dd65e1bd
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 10:10:49 -0300

    Create 1.2-HTAP.sql

---
+++ 1.2-HTAP.sql
@@ -0,0 +1,15 @@
+/*
+**  Hybrid transactional and analytical processing (HTAP)
+**  tool to be able to analyse data without affecting transactional operations.
+**  
+**  data warehouses
+**      Exported from the ransactional database
+**      imported into a data warehouse using ETL
+**      tested and maintained
+**      delayed instead of immediate acess.
+**  
+**  Microsoft SQL Server 2019 - HTAP
+**      memory-optimized tables
+**      natively compiled stored procedures
+**      Clustered Columnstore Indexes.
+*/
