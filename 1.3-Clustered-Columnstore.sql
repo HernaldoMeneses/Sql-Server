7e5c4e7281019db2d043c2cd4f4ecc3dd1b1ec06
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-05-02 16:26:16 -0300

    Created 1.3-Clustered-Columnstore.sql

---
+++ 1.3-Clustered-Columnstore.sql
@@ -0,0 +1,10 @@
+/*
+    Clustered Columnstore indexes can make a dramatic difference and are the technology
+    used to optimize real-time analytics. They can achieve an order of magnitude
+    performance gain over a normal row table, a dramatic compression of the data,
+    and minimize interference with real-time transaction processing.
+
+    A columnstore has rows and columns, but the data is stored in a column format.
+
+32/489
+*/
